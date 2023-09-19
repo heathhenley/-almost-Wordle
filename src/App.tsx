2a097bab94cf8a29389dda44ac04b0517269e1a8
Heath Henley <heath.j.henley@gmail.com>
2023-09-19 17:50:29 -0400

    Navigation fixes, backspaces, stop tab for now

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,7 @@ const GameOverPanel = (
 function App() {
   const [guessGrid, setGuessGrid] = useState(
     Array<string[]>(WORDS).fill(Array<string>(WORD_LENGTH).fill('')))
+  const [lastRowEntered, setLastRowEntered] = useState<number | null>(null)
   const [listOfWords, setListOfWords] = useState<string[]>([])
   const [correctWord, setCorrectWord] = useState("")
   const [gameOver, setGameOver] = useState<TGameState>(TGameState.IN_PROGRESS)
@@ -83,40 +84,44 @@ function App() {
     return false
   }
 
-  const earlyReturnHandleBackspace = (row: number, col: number, flatIdx: number)=> {
+  const earlyReturnHandleBackspace =
+      (row: number, col: number, flatIdx: number) => {
     const atStartOfWord = col === 0
     const atEndOfWord = col === WORD_LENGTH - 1
-    console.log("Handle backspace", atStartOfWord, atEndOfWord)
-    // nothing to backspace at the start of the word
-    if (atStartOfWord) return
 
-    // if we're at the end of the word and there is a letter
-    if (atEndOfWord && guessGrid[row][col] !== ''){
+    // End cases, if we're at the at the start or end of a word, clear that
+    // value in words array, focus stays there 
+    if (atStartOfWord || (atEndOfWord && guessGrid[row][col] !== '')) {
       const newGrid = guessGrid.map((word) =>(word.slice()))
       // clear that value in words array, focus stays here
       newGrid[row][col] = ''
       setGuessGrid(newGrid)
       return
     }
-    
-    // move focus back one input
-    const [prevRow, prevCol] = toRowCol(flatIdx - 1)
-    inputRef?.current[flatIdx - 1]?.focus()
-    // clear that value in words array
-    const newGrid = guessGrid.map((word) =>(word.slice()))
-    newGrid[prevRow][prevCol] = ''
-    setGuessGrid(newGrid)
+    if (guessGrid[row][col] !== '') {
+      // if we have a letter here, clear it, don't move focus
+      const newGrid = guessGrid.map((word) =>(word.slice()))
+      // clear that value in words array
+      newGrid[row][col] = ''
+      setGuessGrid(newGrid)
+    } else {
+      // if we don't have a letter here, move focus back one input, clear that
+      // letter
+      const [prevRow, prevCol] = toRowCol(flatIdx - 1)
+      const newGrid = guessGrid.map((word) =>(word.slice()))
+      newGrid[prevRow][prevCol] = ''
+      setGuessGrid(newGrid)
+      inputRef?.current[flatIdx - 1]?.focus()
+    }
   }
 
-  const earlyReturnHandleEnter = (row: number, col: number, flatIdx: number) => {
-    const completeWordEntered = col === WORD_LENGTH - 1
+  const earlyReturnHandleEnter = (row: number, flatIdx: number) => {
+    const completeWordEntered = guessGrid[row].join('').length === WORD_LENGTH
     const enteredWordIsCorrect = guessGrid[row].join('') === correctWord
     const allGuessesUsed = flatIdx === WORDS * WORD_LENGTH - 1
-    const wordInWordList = listOfWords.includes(
-      guessGrid[row].join(''))
+    const wordInWordList = listOfWords.includes(guessGrid[row].join(''))
     // We need to check for game stuff here
     // - Check if the word is complete and add classes to color if it is
-    console.log(correctWord)
     if (completeWordEntered) {
       if (!wordInWordList) {
         setAlert(TAlert.WORD_NOT_IN_LIST)
@@ -133,6 +138,7 @@ function App() {
         }
         currentInput?.toggleAttribute("readonly", true)
       })
+      setLastRowEntered(row)
     }
     if (completeWordEntered && enteredWordIsCorrect) {
       setGameOver(TGameState.WON)
@@ -148,18 +154,22 @@ function App() {
     inputRef?.current[flatIdx + 1]?.focus()
   }
 
-  const handleBackspaceOrEnter = (e: React.KeyboardEvent<HTMLInputElement>,
+  const handleSpecialKeys = (e: React.KeyboardEvent<HTMLInputElement>,
       row: number, col: number) => {
-    e.preventDefault()
     const flatIdx = toFlatIndex(row, col)
+    if (e.key === 'Tab') {
+      e.preventDefault()
+      return
+    }
     if (e.key === 'Enter') {
-      earlyReturnHandleEnter(row, col, flatIdx)
+      earlyReturnHandleEnter(row, flatIdx)
       return
     }
     if (e.key === 'Backspace') {
       earlyReturnHandleBackspace(row, col, flatIdx)
       return
     }
+    // TODO(Heath): maybe handle tab and arrow keys here for navigation?
   }
 
   const handleInputChanged = (e: React.ChangeEvent<HTMLInputElement>,
@@ -192,13 +202,12 @@ function App() {
     inputRef?.current[flatIdx + 1]?.focus()
   }
 
-  const handleInputFocused = (row: number, col: number) => {
-    const flatIdx = toFlatIndex(row, col)
-    // Are they trying to focus an input that's not allowed?
-    if (earlyReturnEditInvalidInput(flatIdx)) {
-      // Move focus to next valid input
-      inputRef?.current[firstAvailableIndex]?.focus()
-      return
+  const handleInputClicked = (row: number) => {
+    const validEditableRow = lastRowEntered !== null ? lastRowEntered + 1 : 0
+    // If the user clicks on a row that is not the next editable row, move back
+    // to the last editable row, arbitrarily the first letter
+    if (row !== validEditableRow) {
+      inputRef?.current[toFlatIndex(validEditableRow, 0)]?.focus()
     }
   }
 
@@ -213,6 +222,7 @@ function App() {
     setGuessGrid((prev) => prev.map((word) => word.map(() => '')))
     setGameOver(TGameState.IN_PROGRESS)
     setCorrectWord(selectRandomWord(listOfWords))
+    setLastRowEntered(null)
   }
 
   if (!correctWord) {
@@ -234,14 +244,14 @@ function App() {
             <div className="row" key={i}>
               {word.map((l, j) => (
                 <input key={`${i}-${j}`}
-                  onKeyUp={(e) => handleBackspaceOrEnter(e, i, j)}
+                  onKeyDown={(e) => handleSpecialKeys(e, i, j)}
                   type="text"
                   value={l}
                   ref={(ref) => inputRef.current[toFlatIndex(i, j)] = ref}
                   maxLength={1}
                   autoFocus={i === 0 && j === 0}
                   onChange={(e) => handleInputChanged(e, i, j)}
-                  onFocus={() => handleInputFocused(i, j)}
+                  onClick={() => handleInputClicked(i)}
                   />
                 ))}
             </div>))}
